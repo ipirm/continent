5ed807842b73d215bdecc9bdc9b8e16f152f54f8
Ilham <48025760+ipirm@users.noreply.github.com>
2019-08-15 22:18:33 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 const swiper = new Swiper('#js-swiper__inner', {
-	spaceBetween: 30,
-	effect: 'fade',
+	spaceBetween: 0,
+	loop: true,
 	slidesPerView: 1,
 	allowTouchMove: false,
 	autoplay: true
+});
+
+const swiper2 = new Swiper('#js-swiper-outer', {
+
+	spaceBetween: 0,
+	loop: true,
+	slidesPerView: 3,
+	slidesPerGroup: 3,
+	allowTouchMove: false,
+	autoplay: true,
+	breakpoints: {
+  
+    600: {
+      slidesPerView: 1,
+     slidesPerGroup: 1,
+    },
+   },
 });
